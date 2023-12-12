d63d0c45aceb0e70692085804aac4f2151d1d7ac
Роман Носов <romannosov_2001@mail.ru>
2023-12-12 18:30:45 +0300

    Add entities

---
+++ src/main/java/ru/netology/Cloud/entities/Role.java
@@ -0,0 +1,18 @@
+package ru.netology.Cloud.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
