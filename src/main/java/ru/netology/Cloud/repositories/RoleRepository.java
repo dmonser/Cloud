a24719ab4ccd155e4263891f79ddfa90f926aea5
Роман Носов <romannosov_2001@mail.ru>
2023-12-13 18:51:32 +0300

    Add Reposytories and Service

---
+++ src/main/java/ru/netology/Cloud/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package ru.netology.Cloud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.netology.Cloud.entities.Role;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
