12274cf0951c5cf025ab536d7016262ead74fd81
Roman Nosov <romannosov_2001@mail.ru>
2023-12-18 01:22:22 +0300

    Add utils

---
+++ src/main/java/ru/netology/Cloud/utils/JwtTokenUtils.java
@@ -0,0 +1,36 @@
+package ru.netology.Cloud.utils;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenUtils {
+    @Value("${app.jwt-secret}")
+    private String secret;
+
+    @Value("${jwt.lifetime}")
+    private Duration jwtLifetime;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        List<String> rolesList = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        claims.put("roles", rolesList);
+
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + jwtLifetime.toMillis());
+
+        return Jwts.builder()
+    }
+}
