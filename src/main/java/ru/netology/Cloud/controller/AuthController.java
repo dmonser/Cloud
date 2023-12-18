cd1f1fbe1fa6a5bc547815ac1f40b6bfda306e78
Роман Носов <romannosov_2001@mail.ru>
2023-12-18 17:38:49 +0300

    Add AuthController, JwtRequest, JwtResponse, AppError. Update JwtTokenUtils

---
+++ src/main/java/ru/netology/Cloud/controller/AuthController.java
@@ -0,0 +1,38 @@
+package ru.netology.Cloud.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.Cloud.dtos.JwtRequest;
+import ru.netology.Cloud.dtos.JwtResponse;
+import ru.netology.Cloud.exceptions.AppError;
+import ru.netology.Cloud.service.UserService;
+import ru.netology.Cloud.utils.JwtTokenUtils;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final JwtTokenUtils jwtTokenUtils;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Некорректный логин или пароль"), HttpStatus.UNAUTHORIZED);
+        }
+
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+        String tocken = jwtTokenUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(tocken));
+    }
+}
--- src/main/java/ru/netology/Cloud/utils/JwtTokenUtils.java
+++ src/main/java/ru/netology/Cloud/utils/JwtTokenUtils.java
@@ -1,6 +1,8 @@
 package ru.netology.Cloud.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -32,5 +34,27 @@ public class JwtTokenUtils {
         Date expiredDate = new Date(issuedDate.getTime() + jwtLifetime.toMillis());
 
         return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(issuedDate)
+                .expiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public List<String> getRoles(String token) {
+        return getAllClaimsFromToken(token).get("roles", List.class);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
