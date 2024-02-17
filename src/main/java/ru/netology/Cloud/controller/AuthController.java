ba39bff1f200f6b0692f8bb964f3d07045f8b967
Roman Nosov <romannosov_2001@mail.ru>
2024-02-17 15:25:32 +0300

    Refactoring AuthController

--- src/main/java/ru/netology/Cloud/controller/AuthController.java
+++ src/main/java/ru/netology/Cloud/controller/AuthController.java
@@ -2,29 +2,27 @@ package ru.netology.Cloud.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.Cloud.dto.LoginRequest;
+import ru.netology.Cloud.dto.LoginResponse;
 import ru.netology.Cloud.service.AuthServiceImpl;
 
-//@RequestMapping("/cloud")
+@RequestMapping
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthServiceImpl authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         final var response = authService.login(loginRequest);
         return ResponseEntity.ok(response);
     }
 
-    // TODO: 30.01.2024 Logout
+
     @PostMapping("/logout")
-    public ResponseEntity<?> logout(@RequestHeader("auth-token") String authToken) {
+    public ResponseEntity<Void> logout(@RequestHeader("auth-token") String authToken) {
         authService.logout(authToken);
-        return ResponseEntity.ok().body("logout");
+        return ResponseEntity.ok().build();
     }
 }
