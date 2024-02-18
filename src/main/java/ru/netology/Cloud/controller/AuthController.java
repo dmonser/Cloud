e1091555ce04c41c6d164a12b91c0318702fb3bb
Roman Nosov <romannosov_2001@mail.ru>
2024-02-18 14:45:14 +0300

    Add logger into the controllers

--- src/main/java/ru/netology/Cloud/controller/AuthController.java
+++ src/main/java/ru/netology/Cloud/controller/AuthController.java
@@ -1,6 +1,7 @@
 package ru.netology.Cloud.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.Cloud.dto.LoginRequest;
@@ -10,19 +11,21 @@ import ru.netology.Cloud.service.AuthServiceImpl;
 @RequestMapping
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
     private final AuthServiceImpl authService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         final var response = authService.login(loginRequest);
+        log.info(String.format("User '%s' success login", loginRequest.login()));
         return ResponseEntity.ok(response);
     }
-
-
+    
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(@RequestHeader("auth-token") String authToken) {
         authService.logout(authToken);
+        log.info("User success logout");
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ru/netology/Cloud/controller/DataController.java
+++ src/main/java/ru/netology/Cloud/controller/DataController.java
@@ -1,6 +1,7 @@
 package ru.netology.Cloud.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import java.util.List;
 @RequestMapping
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class DataController {
 
     private final FileServiceImpl fileService;
@@ -26,18 +28,21 @@ public class DataController {
     @PostMapping("/file")
     public ResponseEntity<Void> fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         fileService.saveFile(file);
+        log.info(String.format("File '%s' success upload", file.getOriginalFilename()));
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/file")
     public ResponseEntity<Void> fileDelete(@RequestParam("filename") String fileName) {
         fileService.deleteFile(fileName);
+        log.info(String.format("File '%s' success delete", fileName));
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/file")
     public ResponseEntity<ByteArrayResource> fileDownload(@RequestParam("filename") String fileName) {
         File file = fileService.getFile(fileName);
+        log.info(String.format("File '%s' success download", fileName));
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(file.getContentType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getOriginalName() + "\"")
@@ -47,12 +52,14 @@ public class DataController {
     @PutMapping("/file")
     public ResponseEntity<Void> editFileName(@RequestParam("filename") String filename, @RequestBody PutRequest putRequest) {
         fileService.editFileName(filename, putRequest.filename());
+        log.info(String.format("File name '%s' success edit to file name '%s'", filename, putRequest.filename()));
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/list")
     public ResponseEntity<List<FileResponse>> getFilesList(@RequestParam("limit") int limit) {
         List<FileResponse> result = fileService.getFileResponseList(limit);
+        log.info("File list success get");
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
